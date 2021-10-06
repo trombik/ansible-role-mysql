b14383fc4da591a06d8b036635418d2e457903f6
Tomoyuki Sakurai <y@trombik.org>
2021-10-07 02:25:45 +0700

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,48 +2,60 @@ require "spec_helper"
 require "serverspec"
 
 package = "mysql"
-service = "mysql"
-config  = "/etc/mysql/mysql.conf"
+service = "mysql-server"
+config_dir = "/etc/mysql"
 user    = "mysql"
 group   = "mysql"
-ports   = [PORTS]
-log_dir = "/var/log/mysql"
-db_dir  = "/var/lib/mysql"
+ports   = [3306]
+default_user = "root"
+default_group = "root"
+default_random_password_file = nil
 
 case os[:family]
 when "freebsd"
-  config = "/usr/local/etc/mysql.conf"
-  db_dir = "/var/db/mysql"
+  package = "mysql57-server"
+  config_dir = "/usr/local/etc/mysql"
+  default_group = "wheel"
+  default_random_password_file = "/root/.mysql_secret"
 end
+config  = "#{config_dir}/my.cnf"
 
 describe package(package) do
   it { should be_installed }
 end
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("mysql") }
-end
-
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+  it { should be_mode 644 }
+  its(:content) { should match Regexp.escape("Managed by ansible") }
 end
 
 case os[:family]
 when "freebsd"
-  describe file("/etc/rc.conf.d/mysql") do
+  describe file("/etc/rc.conf.d/mysql_server") do
+    it { should exist }
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
   end
+end
+
+if default_random_password_file
+  describe file default_random_password_file do
+    it { should_not exist }
+  end
+end
+
+describe file "/root/.my.cnf" do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 600 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(/Managed by ansible/) }
+  its(:content) { should match(/# mysql_extra_my_cnf_config/) }
 end
 
 describe service(service) do
@@ -55,4 +67,10 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe command "mysql --user=root --execute 'SELECT 42;'" do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/^42$/) }
 end
